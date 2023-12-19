d18500de104f8abd58a96b0f56d7285bb8a125b7
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-19 23:47:21 +0300

    created draft of project page

---
+++ src/fsd-pages/SpecialProjectPage/SpecialProjectPage.tsx
@@ -0,0 +1,36 @@
+import { END_POINTS } from "@/shared/constants/endPoints"
+import styles from './SpecialProjectPage.module.scss'
+import { ProjectCard } from "@/entities/special-project"
+
+
+interface Project {
+  id: number
+  name: string
+  type: string
+  description: string
+  image_link: string
+}
+
+const getProject: () => Promise<Project> = () => {
+  return fetch(END_POINTS.getProject).then((response) => {
+    return response.json()
+  })
+}
+
+
+export const SpecialProjectPage = async (): Promise<JSX.Element> => {
+  const projectData = await getProject()
+
+  return (
+    <div
+      className={ styles.page }
+      style={ {
+        backgroundImage: `url("${projectData.image_link}")`
+      } }
+    >
+      <div className={ styles.contentWrapper }>
+        <ProjectCard />
+      </div>
+    </div>
+  )
+}
