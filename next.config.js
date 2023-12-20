66682ab7ddaa78bb1aad0ed7e33750af9349d275
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-20 15:33:18 +0300

    updated config for svg files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: [ 'imageup.ru', ],
   },
+
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [ '@svgr/webpack', ],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
