e3956e20f08a2db879e65b904d2776fc1b7ed376
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-21 00:37:51 +0300

    fix fields access

--- src/shared/constants/endPoints.ts
+++ src/shared/constants/endPoints.ts
@@ -2,15 +2,15 @@ interface Global {
   /**
    * Это хак, так как хостинг неизвестен.
    */
-  __incrementalCache: {
-    requestHeaders: {
-      referer: string
+  __incrementalCache?: {
+    requestHeaders?: {
+      referer?: string
     }
   }
 }
 
 // eslint-disable-next-line no-underscore-dangle
-const DATA_BASE_URL = (global as unknown as Global).__incrementalCache.requestHeaders.referer
+const DATA_BASE_URL = (global as unknown as Global)?.__incrementalCache?.requestHeaders?.referer
 
 
 export const END_POINTS = {
