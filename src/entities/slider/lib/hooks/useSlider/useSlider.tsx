e9512795bad3b643e60dcfd1109bc235ebabdba6
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-20 22:57:27 +0300

    added comments

--- src/entities/slider/lib/hooks/useSlider/useSlider.tsx
+++ src/entities/slider/lib/hooks/useSlider/useSlider.tsx
@@ -10,6 +10,9 @@ import { showPreviousSlide, } from '../../utils/showPreviousSlide'
 
 export const useSlider = () => {
   useEffect(() => {
+    /**
+     * Назначаем обработчики кликов по кнопкам слайдера.
+     */
     const buttonPrev = document.getElementById('previous-slide-button') as HTMLButtonElement
     const buttonNext = document.getElementById('next-slide-button') as HTMLButtonElement
 
@@ -26,11 +29,17 @@ export const useSlider = () => {
 
 
   useEffect(() => {
+    /**
+     * Задаем ширину бегунка при монтировании компонента.
+     */
     setThumbWidth()
   }, [])
 
 
   useEffect(() => {
+    /**
+     * Пересчитываем ширину бегунка и сбрасываем бегунок и поток слайдов в исходное состояние при изменении размера viewport.
+     */
     window.addEventListener('resize', setThumbWidth)
     window.addEventListener('resize', resetSliderTransform)
 
